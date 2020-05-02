36c961687b04da81d7a005283135e2c720f8eb1f
cheater <Cheater.dev@hotmail.com>
2020-05-02 15:09:29 +0300

    Remove usage of DeviceException

--- custom_components/clear_grass/sensor.py
+++ custom_components/clear_grass/sensor.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import click
 import json
 from miio.click_common import command, format_output
-from miio.device import Device, DeviceException
+from miio.device import Device
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -26,10 +26,6 @@ FUNC_PROPERTIES = {
     MODEL_AIRQUALITYMONITOR_S1: "get_prop",
     MODEL_AIRQUALITYMONITOR_B1: "get_air_data"
 }
-
-class AirQualityMonitorException(DeviceException):
-    pass
-
 
 class AirQualityMonitorStatus:
     """Container of air quality monitor status."""
@@ -225,7 +221,7 @@ class AirQualityMonitor(Device):
         end = end_hour * 3600 + end_minute * 60
 
         if begin < 0 or begin > 86399 or end < 0 or end > 86399:
-            AirQualityMonitorException("Begin or/and end time invalid.")
+            raise Exception("Begin or/and end time invalid.")
 
         self.send("set_night_time", [begin, end])
 
@@ -294,7 +290,7 @@ async def async_setup_platform(hass, config, async_add_entities,
                      device_info.hardware_version)
         device = ClearGrassMonitor(
             name, air_quality_monitor, model, unique_id)
-    except DeviceException:
+    except Exception:
         raise PlatformNotReady
 
     hass.data[DATA_KEY][host] = device
